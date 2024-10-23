36f86117c9d01ed85667e4bce3abe5376ff0af30
salomaoalg <sa.salomao@hotmail.com>
2024-10-23 17:08:14 +0100

    inicio

--- README.md
+++
@@ -1,2 +0,0 @@
-# instituto_react
- Site Institucional 
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+'use-client'
+export default function Home(){
+    return(
+        <div>
+            <h1>Site Institucional</h1>
+        </div>
+    )
+}
